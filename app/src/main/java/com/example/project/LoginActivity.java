f1843fec56f075fbb8b8d7338311cc2465f19e60
Joshuapavan <joshuapavan35@gmail.com>
2021-10-17 19:08:34 +0530

    1. Added 2 image buttons for Google and Facebook Signing In.

--- app/src/main/java/com/example/project/LoginActivity.java
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -5,18 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText email, password;
     Button loginButton, skipButton;
+    ImageButton googleButton, facebookButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         assignId();
-
     }
+
     public void assignId(){
         //edit Text
         email = findViewById(R.id.email);
@@ -24,5 +26,8 @@ public class LoginActivity extends AppCompatActivity {
         //Buttons
         loginButton = findViewById(R.id.login);
         skipButton = findViewById(R.id.skip);
+        //image button
+        googleButton =  findViewById(R.id.googleSignIn);
+        facebookButton = findViewById(R.id.facebookSignIn);
     }
 }
